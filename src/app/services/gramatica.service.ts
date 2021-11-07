4621e7e55f4525c7a19f8326dd742f023c6f451f
Miguel-Restrepo <miguel.1701911153@ucaldas.edu.co>
2021-11-07 14:17:28 -0500

    cargar gramatica

--- src/app/services/gramatica.service.ts
+++ src/app/services/gramatica.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ServiceConfig } from 'src/app/config/service.config';
+import { ArchivoModel } from 'src/app/modelos/archivo.model'
 @Injectable({
   providedIn: 'root'
 })
 export class GramaticaService {
-
-  constructor() { }
+  entity: String='CargarRIPS';
+  constructor(
+    private http: HttpClient
+  ) { 
+    
+  }
+  cargarArchivo(formData:FormData): Observable<ArchivoModel>{
+    return this.http.post<ArchivoModel>( `${ServiceConfig.BASE_URL}${this.entity}`,formData, {
+      headers: new HttpHeaders({})
+  })
+}
 }

52653214bfc52090220ba5705842376fb189060a
Julian Florez <julian.1701913963@ucaldas.edu.co>
2021-11-07 20:30:42 -0500

    Vers

--- src/app/modules/gramatica/ver-gramatica/ver-gramatica.component.ts
+++ src/app/modules/gramatica/ver-gramatica/ver-gramatica.component.ts
@@ -1,36 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { ActivatedRoute, Router } from '@angular/router';
+import { GramaticaService } from 'src/app/services/gramatica.service';
 @Component({
   selector: 'app-ver-gramatica',
   templateUrl: './ver-gramatica.component.html',
-  styleUrls: ['./ver-gramatica.component.css']
+  styleUrls: ['./ver-gramatica.component.css'],
 })
-
-
 export class VerGramaticaComponent implements OnInit {
-   ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-  constructor() { }
+  recordId: string;
+  primeros: any = [];
+  siguientes: any = [];
+  prediccion: any = [];
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private servicio: GramaticaService
+  ) {
+    this.recordId = this.route.snapshot.params['id'];
+  }
 
   ngOnInit(): void {
+    this.obtenerPrimeros();
+    this.obtenerSiguientes();
+    this.obtenerPrediccion();
   }
-  displayedColumns: string[] = [ 'NoTerminal', 'Tipo'];
-  primeros = this.ELEMENT_DATA;
-  siguientes = this.ELEMENT_DATA;
-  prediccion = this.ELEMENT_DATA;
+
+  obtenerPrimeros() {
+    this.servicio.obtenerPrimeros(Number(this.recordId)).subscribe(
+      (data) => {
+        this.primeros = data;
+        console.log(data);
+      },
+      (err) => {
+        console.log('No se encontro ');
+      }
+    );
+  }
+
+  obtenerSiguientes() {
+    this.servicio.obtenerSiguientes(Number(this.recordId)).subscribe(
+      (data) => {
+        this.siguientes = data;
+        console.log(data);
+      },
+      (err) => {
+        console.log('No se encontro ');
+      }
+    );
+  }
+
+  obtenerPrediccion() {
+    this.servicio.obtenerCP(Number(this.recordId)).subscribe(
+      (data) => {
+        this.prediccion = data;
+        console.log(data);
+      },
+      (err) => {
+        console.log('No se encontro ');
+      }
+    );
+  }
+
+  displayedColumns: string[] = ['NoTerminal', 'Tipo'];
 }
